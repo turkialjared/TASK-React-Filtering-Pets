49c3961314c7d20b05339f4c8482525b30ce5368
turki almutairi <turkeyaljared@hotmail.com>
2023-12-21 20:46:56 +0300

    w4d2

--- src/Components/PetItem.js
+++ src/Components/PetItem.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 function PetItem({ pet }) {
+  const [imageChange, setimageChange] = useState(pet.image);
+
+  const change = () => {
+    setimageChange(pet.image2);
+  };
+
   return (
     <div className="col-lg-4 col-md-8 col-sm-10">
       <div className="single-doctor">
-        <img className="image" alt={pet.name} src={pet.image} />
+        <img className="image" alt={pet.name} src={imageChange} />
         <div className="content">
           <h3>{pet.name}</h3>
-          <button type="button" className="btn btn-info">
+          <button onClick={change} type="button" className="btn btn-info">
             Pet
           </button>
           <button type="button" class="btn btn-info  m-2">
--- src/Components/PetsList.js
+++ src/Components/PetsList.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import pets from "../petsData";
 import PetItem from "./PetItem";
+import petsData from "../petsData";
 
 function PetsList() {
-  const petList = pets.map((pet) => <PetItem pet={pet} key={pet.id} />);
+  const [query, setquery] = useState("");
+  const petList = pets
+    .filter((pet) => pet.name.toLowerCase().includes(query.toLowerCase()))
+    .map((pet) => <PetItem pet={pet} key={pet.id} />);
 
+  const search = (e) => {
+    setquery(e.target.value);
+  };
   return (
     <section id="doctors" className="doctor-section pt-140">
       <div className="container">
@@ -15,6 +23,7 @@ function PetsList() {
               </h1>
               <div className="input-group rounded">
                 <input
+                  onChange={search}
                   type="search"
                   className="form-control rounded"
                   placeholder="Search"
